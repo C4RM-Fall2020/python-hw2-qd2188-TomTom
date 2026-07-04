62382a1a8627c20ddc2a94d2d5fdc56d0380c4c6
qd2188-TomTom <qd2188@columbia.edu>
2026-07-03 19:45:17 -0700

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -3,10 +3,13 @@ import numpy as np
 def getBondPrice(y, face, couponrate, m, ppy=1):
     totalperiods = m * ppy 
     t = np.arange(1,totalperiods + 1)
+    
     coupon = face * couponrate / ppy
     cf = np.ones(totalperiods) * coupon
     cf[-1] = cf[-1] + face
+    
     pv = (1+y / ppy) ** (-t)
     pvcf = pv * cf
+    
     bondprice = np.sum(pvcf)
     return(bondprice)
