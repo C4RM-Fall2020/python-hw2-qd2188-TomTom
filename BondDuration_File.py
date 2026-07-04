682b959c281e58b05ac68019653f8bc7f1006f10
qd2188-TomTom <qd2188@columbia.edu>
2026-07-03 21:02:19 -0700

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -2,28 +2,16 @@ import numpy as np
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
     totalperiods = m * ppy
-
-    
     t = np.arange(1, totalperiods + 1)
     time = t / ppy
-
-    
     coupon = face * couponRate / ppy
     cf = np.ones(totalperiods) * coupon
     cf[-1] = cf[-1] + face 
-
-
     pv = (1 + y / ppy) ** (-t)
     pvcf = cf * pv
-
-
     bondprice = np.sum(pvcf)
-
-
     weightedtime = np.sum(time * pvcf)
-
     bondduration = weightedtime / bondprice
-
-return(bondduration)
+    return(bondduration)
     
     
