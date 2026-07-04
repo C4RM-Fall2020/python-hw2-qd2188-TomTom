fbda4b726aa49b5d11ee133efa20a915b66d01e3
qd2188-TomTom <qd2188@columbia.edu>
2026-07-03 21:20:36 -0700

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,11 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
-    return(v)
+    nums = np.arange(start, finish + 1)
+
+    outlist = np.arange(start, finish + 1, dtype=object)
+    outlist[nums % 3 == 0] = "fizz"
+    outlist[nums % 5 == 0] = "buzz"
+    outlist[nums % 15 == 0] = "fizzbuzz"
+
+    return(outlist)
